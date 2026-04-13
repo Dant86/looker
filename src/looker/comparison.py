090ceee36554d2f9a8331e19d4a26421b77e159a
Vedant Pathak <vedantdpathak@iCloud.com>
2026-04-13 14:35:17 -0400

    fix trailing whitespace

--- src/looker/comparison.py
+++ src/looker/comparison.py
@@ -16,8 +16,8 @@ def jensen_shannon_divergence(
         A tensor of shape (1,).
     """
     mixture = (p + q) / 2
-    
+
     kl_p_mixture = p @ (torch.log(p) - torch.log(mixture))
     kl_q_mixture = q @ (torch.log(q) - torch.log(mixture))
-    
+
     return (kl_p_mixture + kl_q_mixture) / 2
